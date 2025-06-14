f489a924e426af6bab4997a77e37bf3f0028395c
felipeFerreiraffl <felipado2x0@gmail.com>
2025-06-13 23:14:39 -0300

    feat: first test by tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <p className="text-base font-bold">Texto</p>
+    </div>
+  );
+}
